a4ce9fccbd06be4369488c284d9cfdb602c1da76
shulman33 <sshulma5@mail.yu.edu>
2022-05-17 22:15:45 -0400

    check

--- src/bot.js
+++ src/bot.js
@@ -44,10 +44,11 @@ if (date[0] != '0'){
 }
 
 let newDateAndTime = newTime + newDate;
+
+
 //console.log(newDateAndTime);
 //"1 30 16 31 3 *"
-  schedule.scheduleJob(newDateAndTime, async() => {
-    console.time("timer");
+  (async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.setViewport({ width: 1366, height: 768});
@@ -66,35 +67,42 @@ let newDateAndTime = newTime + newDate;
     await page.click('a.submenulinktext2 ');
     await page.goto('https://banner.oci.yu.edu/ssb/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu');
     await page.goto('https://banner.oci.yu.edu/ssb/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu');
-    await page.goto('https://banner.oci.yu.edu/ssb/bwskfreg.P_AltPin');
-    await page.click('#term_id');
-    
-    await page.select("select#term_id","202209");
-    await page.click('#term_id');
 
-    const  [aresponse] = await Promise.all([
-      page.waitForNavigation(),
-      page.click('[value="Submit"]')
-    ]);
+    schedule.scheduleJob(newDateAndTime, async() =>{
+      console.time("timer");
 
-    await page.type('#crn_id1', crn1);
+      await page.goto('https://banner.oci.yu.edu/ssb/bwskfreg.P_AltPin');
+      await page.click('#term_id');
 
-    try {
-      await page.type('#crn_id2', crn2);
-      await page.type('#crn_id3', crn3);
-      await page.type('#crn_id4', crn4);
-      await page.type('#crn_id5', crn5);
-      await page.type('#crn_id6', crn6);
-      await page.type('#crn_id7', crn7);
-      await page.type('#crn_id8', crn8);
-      await page.type('#crn_id9', crn9);
-      await page.type('#crn_id10', crn10);
-    } catch (error) {
-      await page.keyboard.press('Enter');
-    }
-    console.timeEnd("timer");
-    await page.waitForNavigation();
-    await page.screenshot({path: './registration-status.png'});
-    await browser.close();
-    
-  })
+      await page.select("select#term_id","202209");
+      await page.click('#term_id');
+
+      const  [aresponse] = await Promise.all([
+        page.waitForNavigation(),
+        page.click('[value="Submit"]')
+      ]);
+
+      await page.type('#crn_id1', crn1);
+
+      try {
+        await page.type('#crn_id2', crn2);
+        await page.type('#crn_id3', crn3);
+        await page.type('#crn_id4', crn4);
+        await page.type('#crn_id5', crn5);
+        await page.type('#crn_id6', crn6);
+        await page.type('#crn_id7', crn7);
+        await page.type('#crn_id8', crn8);
+        await page.type('#crn_id9', crn9);
+        await page.type('#crn_id10', crn10);
+      } catch (error) {
+        await page.keyboard.press('Enter');
+      }
+      console.timeEnd("timer");
+      await page.waitForNavigation();
+      await page.screenshot({path: './registration-status.png'});
+      await browser.close();
+    })
+
+  })();
+
+
